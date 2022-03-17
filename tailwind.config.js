2f8975cb8f336fe791c71cbd9d86360dd644e461
Sebastian Ojefors <godnoken@gmail.com>
2022-03-17 12:32:24 +0100

    Add desktop-layout MVP

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,15 @@ module.exports = {
       fontSize: {
         appHeader: "clamp(20px, 4 * (1vw + 1vh) / 2, 28px)",
         containerHeader: "clamp(24px, 4 * (1vw + 1vh) / 2, 30px)",
-        cardH: "clamp(12px, 2.8 * (1vw + 1vh) / 2, 22px)",
-        cardP: "clamp(12px, 2.2 * (1vw + 1vh) / 2, 18px)",
+        cardH: "clamp(12px, 2.8 * (1vw + 1vh) / 2, 20px)",
+        cardP: "clamp(12px, 2.2 * (1vw + 1vh) / 2, 16px)",
       },
       boxShadow: {
         card: "rgba(0, 0, 0, 0.24) 0px 3px 8px"
       },
       spacing: {
-        bigIcon: "clamp(24px, 7 * (1vw + 1vh) / 2, 60px)",
-        smallIcon: "clamp(18px, 4 * (1vw + 1vh) / 2, 60px)",
+        bigIcon: "clamp(24px, 7 * (1vw + 1vh) / 2, 45px)",
+        smallIcon: "clamp(18px, 4 * (1vw + 1vh) / 2, 30px)",
         mainTopPadding: "clamp(18px, 12 * (1vw + 1vh) / 2, 200px)",
         mainBotPadding: "clamp(18px, 22 * (1vw + 1vh) / 2, 225px)",
       }
